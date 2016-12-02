3f8adb6fa36b9d25044082a32361cb1ddb02910f
jmatj <jonas.matter@bluewin.ch>
2016-12-02 16:54:43 +0100

    add usercontrol for tabs

---
+++ ch.hsr.wpf.gadgeothek.ui/tab/GadgetTab.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ch.hsr.wpf.gadgeothek.ui.tab
+{
+    /// <summary>
+    /// Interaction logic for GadgetTab.xaml
+    /// </summary>
+    public partial class GadgetTab : UserControl
+    {
+        public GadgetTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
