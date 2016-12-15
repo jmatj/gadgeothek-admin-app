86c5f27b3a2fe318a705150961cc7e20ff540fca
Robin Suter <suter.robin@gmail.com>
2016-12-15 16:38:08 +0100

    Added Refresh Button to Loans Tab

--- ch.hsr.wpf.gadgeothek.ui/viewmodel/LoanVM.cs
+++ ch.hsr.wpf.gadgeothek.ui/viewmodel/LoanVM.cs
@@ -1,5 +1,6 @@
 ﻿using ch.hsr.wpf.gadgeothek.domain;
 using ch.hsr.wpf.gadgeothek.service;
+using ch.hsr.wpf.gadgeothek.ui.command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,16 +8,29 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace ch.hsr.wpf.gadgeothek.ui.viewmodel
 {
     class LoanVM : BindableBase
     {
         private LibraryAdminService libraryAdminService;
+
         public ObservableCollection<Loan> Loans { get; set; }
 
+        public ICommand RefreshCommand { get; set; }
+
+        private bool _isRefreshing;
+        public bool IsRefreshing
+        {
+            get { return _isRefreshing; }
+            set { SetProperty(ref _isRefreshing, value, nameof(IsRefreshing)); }
+        }
         public LoanVM()
         {
+            RefreshCommand = new RelayCommand(() => Refresh(), () => true);
+
             LoadData();
         }
 
@@ -33,6 +47,18 @@ namespace ch.hsr.wpf.gadgeothek.ui.viewmodel
             {
                 Loans.Add(loan);
             }
+        }
+        private void Refresh()
+        {
+            IsRefreshing = true;
+            Task.Run(() =>
+            {
+                LoadLoans();
+                Dispatcher.CurrentDispatcher.Invoke(() =>
+                {
+                    IsRefreshing = false;
+                });
+            });
         }
     }
 }
