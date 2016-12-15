410b05fb6a10ddc49585b69e1f13a13e891ccec6
jmatj <jonas.matter@bluewin.ch>
2016-12-15 15:31:21 +0100

    add loan view model

---
+++ ch.hsr.wpf.gadgeothek.ui/viewmodel/LoanVM.cs
@@ -0,0 +1,38 @@
+﻿using ch.hsr.wpf.gadgeothek.domain;
+using ch.hsr.wpf.gadgeothek.service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch.hsr.wpf.gadgeothek.ui.viewmodel
+{
+    class LoanVM : BindableBase
+    {
+        private LibraryAdminService libraryAdminService;
+        public ObservableCollection<Loan> Loans { get; set; }
+
+        public LoanVM()
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            libraryAdminService = new LibraryAdminService(ConfigurationManager.AppSettings["server"]);
+            LoadLoans();
+        }
+
+        private void LoadLoans()
+        {
+            Loans = new ObservableCollection<Loan>();
+            foreach (Loan loan in libraryAdminService.GetAllLoans())
+            {
+                Loans.Add(loan);
+            }
+        }
+    }
+}
