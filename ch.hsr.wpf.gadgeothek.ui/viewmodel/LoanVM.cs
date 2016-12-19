3012096d042c1dbc2fe57ad7f063329971dc8210
Robin Suter <suter.robin@gmail.com>
2016-12-19 17:13:14 +0100

    Fix Ausleihe Tab refresh, but SelectedItem is lost after refresh

--- ch.hsr.wpf.gadgeothek.ui/viewmodel/LoanVM.cs
+++ ch.hsr.wpf.gadgeothek.ui/viewmodel/LoanVM.cs
@@ -17,45 +17,59 @@ namespace ch.hsr.wpf.gadgeothek.ui.viewmodel
     {
         private LibraryAdminService libraryAdminService;
 
-        public ObservableCollection<Loan> Loans { get; set; }
+        private ObservableCollection<Loan> _loans;
+        public ObservableCollection<Loan> Loans
+        {
+            get { return _loans; }
+            set { SetProperty(ref _loans, value, nameof(Loans)); }
+        }
 
-        public ICommand RefreshCommand { get; set; }
-
+        private Loan _selectedLoan;
+        public Loan SelectedLoan
+        {
+            get { return _selectedLoan; }
+            set { SetProperty(ref _selectedLoan, value, nameof(SelectedLoan)); }
+        }
         private bool _isRefreshing;
         public bool IsRefreshing
         {
             get { return _isRefreshing; }
             set { SetProperty(ref _isRefreshing, value, nameof(IsRefreshing)); }
         }
+        public ICommand RefreshCommand { get; set; }
+
         public LoanVM()
         {
             RefreshCommand = new RelayCommand(() => Refresh(), () => true);
-
+            Loans = new ObservableCollection<Loan>();
             LoadData();
         }
 
         private void LoadData()
         {
             libraryAdminService = new LibraryAdminService(ConfigurationManager.AppSettings["server"]);
-            LoadLoans();
+            LoadLoans(Loans);
         }
 
-        private void LoadLoans()
+        private void LoadLoans(Collection<Loan> targetCollection)
         {
-            Loans = new ObservableCollection<Loan>();
             foreach (Loan loan in libraryAdminService.GetAllLoans())
             {
-                Loans.Add(loan);
+                targetCollection.Add(loan);
             }
         }
         private void Refresh()
         {
             IsRefreshing = true;
+            int selectedIndex = Loans.IndexOf(SelectedLoan);
             Task.Run(() =>
             {
-                LoadLoans();
+                var refreshedLoans = new ObservableCollection<Loan>();
+                LoadLoans(refreshedLoans);
                 Dispatcher.CurrentDispatcher.Invoke(() =>
                 {
+                    Loans = refreshedLoans;
+                    SelectedLoan = Loans.ElementAt(selectedIndex);
                     IsRefreshing = false;
                 });
             });
